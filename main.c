750bb2aace430938bac7747aae3aa6ea53081f48
ppaksso <113421378+ppaksso@users.noreply.github.com>
2022-11-01 17:07:38 +0900

    04

--- main.c
+++ main.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5
+#define SIZE 4
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void square_array(int a[],int size);
+void print_array(int a[],int size);
 
 int main(int argc, char *argv[]) {
 	
-	int grade[SIZE];
-	int i;
-	int score[SIZE];
+	int list[SIZE]= {1,2,3,4};
 	
-	printf("5명의 점수를 입력하세요.\n");
+	print_array(list,SIZE);
+	square_array(list,SIZE);//배열의 값을 바꾸고  
+	print_array(list,SIZE);//바뀐 값을 출력  
 	
-	for(i=0;i<SIZE;i++){
-	 grade[i] = rand() % 100;
-	}
-	
-	for(i=0;i<SIZE;i++){
-	   grade[i]=score[i];
-    }
-	for(i=0;i<SIZE;i++)
-	  printf("score[%i] = %d (%d)\n",i,score[i],grade[i]); 
-    
 	return 0;
 }
+	void square_array(int a[],int size)
+	{
+		int i;
+		
+		for(i=0;i<size;i++)
+		  a[i]=a[i]*a[i];
+
+	}
+	
+    void print_array(int a[],int size)
+	{
+		int i;
+			
+		for(i=0;i<size;i++)
+		  printf("%3d",a[i]);
+	  printf("\n");
+    }
